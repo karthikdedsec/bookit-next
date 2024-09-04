68787923aef5af41888754fbcc845331d04eea53
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-09-04 17:19:06 +0400

    create get all rooms route

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,7 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
+import Room from "../models/room";
 
 export const allRooms = async (req: NextRequest) => {
+  const resPerPage: number = 8;
+  const rooms = await Room.find();
+
   return NextResponse.json({
-    data: "hello",
+    rooms: rooms,
+    resPerPage,
+  });
+};
+
+//create new room
+export const newRoom = async (req: NextRequest) => {
+  const body = await req.json();
+
+  const room = await Room.create(body);
+
+  return NextResponse.json({
+    success: true,
+    room,
   });
 };
